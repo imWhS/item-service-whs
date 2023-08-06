333e0563b7d66761a59244d1ea19cc97fb3f9b6d
Wonhee Son <dnjsgml1230@gmail.com>
2023-08-06 13:51:21 +0900

    Initial commit

---
+++ src/main/java/hello/itemservicewhs/ItemServiceWhsApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservicewhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceWhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceWhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/itemservicewhs/ItemServiceWhsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicewhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceWhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
